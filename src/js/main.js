41fb16b5405f344ed100cd44c0f6918f9e3596ca
Alya S <Alya.SerraFolly@studentfr.ch>
2023-11-07 16:18:55 +0100

    Parallax - test 1

--- src/js/main.js
+++ src/js/main.js
@@ -15,3 +15,51 @@ gsap.to("#ysa", {
   },
   scale: 1.2,
 });
+
+gsap.from("#portal", {
+  scrollTrigger: {
+    markers: true,
+    scrub: true,
+  },
+  top: "200px",
+});
+
+gsap.to("#tree-left", {
+  scrollTrigger: {
+    markers: true,
+    scrub: true,
+  },
+  top: "00px",
+});
+
+gsap.to("#tree-right", {
+  scrollTrigger: {
+    markers: true,
+    scrub: true,
+  },
+  top: "300px",
+});
+
+gsap.to("#castle", {
+  scrollTrigger: {
+    markers: true,
+    scrub: true,
+  },
+  top: "900px",
+});
+
+gsap.to("#foreground", {
+  scrollTrigger: {
+    markers: true,
+    scrub: true,
+  },
+  top: "500px",
+});
+
+gsap.to("#midground", {
+  scrollTrigger: {
+    markers: true,
+    scrub: true,
+  },
+  top: "200px",
+});
